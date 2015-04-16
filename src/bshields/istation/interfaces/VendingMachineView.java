6870b290fd832b89b5ffdcb77fb765eadb56860e
lithl <blanketyblankman@gmail.com>
2015-04-16 17:35:28 -0500

    Implement machine view

--- src/bshields/istation/interfaces/VendingMachineView.java
+++ src/bshields/istation/interfaces/VendingMachineView.java
@@ -1,5 +1,10 @@
 package bshields.istation.interfaces;
 
+/**
+ * Shows all necessary information on the vending machine
+ * 
+ * @author Brian
+ */
 public interface VendingMachineView {
-
+	void showVendingMachine();
 }
--- src/bshields/istation/views/VendingMachineConsoleView.java
+++ src/bshields/istation/views/VendingMachineConsoleView.java
@@ -1,7 +1,30 @@
 package bshields.istation.views;
 
+import static java.lang.System.out;
+
+import java.text.NumberFormat;
+
+import bshields.istation.interfaces.VendingMachine;
 import bshields.istation.interfaces.VendingMachineView;
 
 public class VendingMachineConsoleView implements VendingMachineView {
+	private VendingMachine machine;
+	
+	public VendingMachineConsoleView(VendingMachine machine) { this.machine = machine; }
+
+	@Override
+	public void showVendingMachine() {
+		machine.getShelves().stream()
+			.forEach(shelf -> {
+				shelf.getSlots().stream().forEach(slot -> {
+					out.printf("[(%s) %s :: %s]  ",
+						slot.getKeyCode(),
+						NumberFormat.getCurrencyInstance().format(slot.getPrice().toString()),
+						slot.peek().getName());
+				});
+				out.println();
+			});
+						
+	}
 
 }
