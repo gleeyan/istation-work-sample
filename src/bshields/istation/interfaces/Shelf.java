9186b8e1a37d5bcfac01c3f1f32bc65b6d8be9fa
lithl <blanketyblankman@gmail.com>
2015-04-16 16:13:54 -0500

    Update comments

--- src/bshields/istation/interfaces/Shelf.java
+++ src/bshields/istation/interfaces/Shelf.java
@@ -2,6 +2,11 @@ package bshields.istation.interfaces;
 
 import java.util.List;
 
+/**
+ * A shelf contains a number of shelf slots, and has a given height.
+ * 
+ * @author Brian
+ */
 public interface Shelf {
 	/**
 	 * Gets a copy of the shelf slots on this shelf. Only a copy is returned; the list is not directly editable.
@@ -24,12 +29,12 @@ public interface Shelf {
 	 */
 	void addSlot(ShelfSlot slot, int index);
 	/**
-	 * Removes a slot from this shelf
+	 * Removes the first instance of the specified slot from this shelf
 	 * @param slot the slot to remove
-	 * @return the number of slots remaining
+	 * @return {@code true} if the specified slot was on this shelf
 	 * @see #removeSlot(int)
 	 */
-	int removeSlot(ShelfSlot slot);
+	boolean removeSlot(ShelfSlot slot);
 	/**
 	 * Removes a slot from a position on the shelf
 	 * @param index the location to remove the slot from
