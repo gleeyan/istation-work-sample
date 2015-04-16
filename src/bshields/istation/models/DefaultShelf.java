07f92b6d08f98cbc47c8698e6d6c1878ebb5b7d6
lithl <blanketyblankman@gmail.com>
2015-04-16 16:15:22 -0500

    Implement default shelf

---
+++ src/bshields/istation/models/DefaultShelf.java
@@ -0,0 +1,41 @@
+package bshields.istation.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bshields.istation.interfaces.Shelf;
+import bshields.istation.interfaces.ShelfSlot;
+
+/**
+ * Default implementation of shelf interface. {@inheritDoc}
+ * @author Brian
+ */
+public class DefaultShelf implements Shelf {
+	private List<ShelfSlot> slots;
+	private int height;
+
+	@Override
+	public List<ShelfSlot> getSlots() { return new ArrayList<ShelfSlot>(slots); }
+
+	@Override
+	public void addSlot(ShelfSlot slot) { slots.add(slot); }
+
+	@Override
+	public void addSlot(ShelfSlot slot, int index) { slots.add(index, slot); }
+
+	@Override
+	public boolean removeSlot(ShelfSlot slot) { return slots.remove(slot); }
+
+	@Override
+	public ShelfSlot removeSlot(int index) { return slots.remove(index); }
+
+	@Override
+	public int getWidth() { return slots.stream().mapToInt(s -> s.getWidth()).sum(); }
+
+	@Override
+	public int getHeight() { return height; }
+
+	@Override
+	public void setHeight(int height) { this.height = height; }
+
+}
