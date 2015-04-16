3edc9e0096c374ed0e58275e39c18a667cdb67e7
lithl <blanketyblankman@gmail.com>
2015-04-16 14:42:08 -0500

    Add comments to vending machine item interfaces

--- src/bshields/istation/interfaces/GraphicalVendingMachineItem.java
+++ src/bshields/istation/interfaces/GraphicalVendingMachineItem.java
@@ -3,8 +3,9 @@ package bshields.istation.interfaces;
 import java.awt.Image;
 
 /**
+ * Graphical vending machine items additionally have a sprite to represent them.
+ * 
  * @author Brian
- *
  */
 public interface GraphicalVendingMachineItem extends VendingMachineItem {
 	Image getSprite();
--- src/bshields/istation/interfaces/VendingMachineItem.java
+++ src/bshields/istation/interfaces/VendingMachineItem.java
@@ -1,5 +1,10 @@
 package bshields.istation.interfaces;
 
+/**
+ * Vending machine items have a name to identify them.
+ * 
+ * @author Brian
+ */
 public interface VendingMachineItem {
 	String getName();
 	void setName(String name);
