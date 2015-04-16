ae0d0670cd86392033cfcc3fd09f8232c46a6632
lithl <blanketyblankman@gmail.com>
2015-04-16 16:39:03 -0500

    Create interface for vending machine items

--- src/bshields/istation/interfaces/ItemView.java
+++ src/bshields/istation/interfaces/ItemView.java
@@ -1,5 +1,10 @@
 package bshields.istation.interfaces;
 
+/**
+ * Used to display {@link VendingMachineItem VendingMachineItems}
+ * 
+ * @author Brian
+ */
 public interface ItemView {
-
+	void showItem();
 }
