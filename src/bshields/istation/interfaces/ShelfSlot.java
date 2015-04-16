258b0fb8ef36af9b91aeb72177d98a31d9c949a7
lithl <blanketyblankman@gmail.com>
2015-04-16 16:27:42 -0500

    Add key code

--- src/bshields/istation/interfaces/ShelfSlot.java
+++ src/bshields/istation/interfaces/ShelfSlot.java
@@ -48,4 +48,7 @@ public interface ShelfSlot {
 	
 	int getWidth();
 	void setWidth(int width);
+	
+	String getKeyCode();
+	void steKeyCode(String keyCode);
 }
--- src/bshields/istation/models/DefaultShelfSlot.java
+++ src/bshields/istation/models/DefaultShelfSlot.java
@@ -15,32 +15,42 @@ public class DefaultShelfSlot implements ShelfSlot {
 	private LinkedList<VendingMachineItem> items;
 	private int price;
 	private int width;
+	private String keyCode;
 	
 	/**
 	 * Creates an empty shelf slot with 0 width and $0.00 price
-	 * @see #DefaultShelfSlot(int, int, List)
+	 * 
+	 * @param keyCode the code for ordering an item from this slot
+	 * @see #DefaultShelfSlot(String, int, int, List)
 	 */
-	public DefaultShelfSlot() { this(0, 0, new LinkedList<VendingMachineItem>()); }
+	public DefaultShelfSlot(String keyCode) { this(keyCode, 0, 0, new LinkedList<VendingMachineItem>()); }
 	/**
 	 * Creates an empty shelf slot with 0 width and a specified price
+	 * 
+	 * @param keyCode the code for ordering an item from this slot
 	 * @param price the price for items in this slot
-	 * @see #DefaultShelfSlot(int, int, List)
+	 * @see #DefaultShelfSlot(String, int, int, List)
 	 */
-	public DefaultShelfSlot(int price) { this(price, 0, new LinkedList<VendingMachineItem>()); }
+	public DefaultShelfSlot(String keyCode, int price) { this(keyCode, price, 0, new LinkedList<VendingMachineItem>()); }
 	/**
 	 * Creates an empty shelf slot with the specified width and price
+	 * 
+	 * @param keyCode the code for ordering an item from this slot
 	 * @param price the price for items in this slot
 	 * @param width the width of this slot
-	 * @see #DefaultShelfSlot(int, int, List)
+	 * @see #DefaultShelfSlot(String, int, int, List)
 	 */
-	public DefaultShelfSlot(int price, int width) { this(price, width, new LinkedList<VendingMachineItem>()); }
+	public DefaultShelfSlot(String keyCode, int price, int width) { this(keyCode, price, width, new LinkedList<VendingMachineItem>()); }
 	/**
 	 * Creates a pre-filled shelf slot
+	 * 
+	 * @param keyCode the code for ordering an item from this slot
 	 * @param price the price for items in this slot
 	 * @param width the width of this slot
 	 * @param items a collection of items to initially fill this slot with
 	 */
-	public DefaultShelfSlot(int price, int width, List<VendingMachineItem> items) {
+	public DefaultShelfSlot(String keyCode, int price, int width, List<VendingMachineItem> items) {
+		this.keyCode = keyCode;
 		this.price = price;
 		this.width = width;
 		this.items = new LinkedList<VendingMachineItem>(items);
@@ -48,13 +58,11 @@ public class DefaultShelfSlot implements ShelfSlot {
 	
 	@Override
 	public void addFirst(VendingMachineItem item) { items.addFirst(item); }
-
 	@Override
 	public void addLast(VendingMachineItem item) { items.addLast(item); }
 
 	@Override
 	public VendingMachineItem removeFirst() { return items.removeFirst(); }
-
 	@Override
 	public VendingMachineItem removeLast() { return items.removeLast(); }
 
@@ -63,13 +71,16 @@ public class DefaultShelfSlot implements ShelfSlot {
 
 	@Override
 	public int getPrice() { return price; }
-
 	@Override
 	public void setPrice(int price) { this.price = price; }
 
 	@Override
 	public int getWidth() { return width; }
-
 	@Override
 	public void setWidth(int width) { this.width = width; }
+	
+	@Override
+	public String getKeyCode() { return keyCode; }
+	@Override
+	public void steKeyCode(String keyCode) { this.keyCode = keyCode; }
 }
