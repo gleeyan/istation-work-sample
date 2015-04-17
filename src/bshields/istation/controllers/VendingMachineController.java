462b0362d5790475939002c86170b4c080b82af5
lithl <blanketyblankman@gmail.com>
2015-04-16 22:52:55 -0500

    Update controller
    * Return messages when requesting items (allows for nice variable length
    return messages, and doesn't have the controller trying to print to
    console)
    * Add missing functionality
    * Clean up imports

--- src/bshields/istation/controllers/VendingMachineController.java
+++ src/bshields/istation/controllers/VendingMachineController.java
@@ -1,33 +1,49 @@
 package bshields.istation.controllers;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 import bshields.istation.interfaces.ShelfSlot;
 import bshields.istation.interfaces.VendingMachine;
-
-import static java.lang.System.out;
+import bshields.istation.interfaces.VendingMachineItem;
+import bshields.istation.interfaces.VendingMachineView;
+import bshields.istation.models.Message;
 
 public class VendingMachineController {
 	private VendingMachine machine;
+	private VendingMachineView view;
 	
-	public VendingMachineController(VendingMachine machine) { this.machine = machine; }
+	public VendingMachineController(VendingMachine machine, VendingMachineView view) {
+		this.machine = machine;
+		this.view = view; 
+	}
 	
-	public void requestItem(String keyCode) {
+	public Message[] requestItem(String keyCode) {
 		ShelfSlot requestedSlot = machine.getShelves().stream()
 			.flatMap(shelf -> shelf.getSlots().stream())
-			.filter(slot -> slot.getKeyCode().equals(keyCode))
+			.filter(slot -> slot.getKeyCode().equalsIgnoreCase(keyCode))
 			.findFirst().get();
 		
 		if (requestedSlot.getPrice().compareTo(machine.getCash()) > 0) {
 			// Customer has put less cash into machine than requested item costs
-			out.println(requestedSlot);
+			return new Message[] { new Message(requestedSlot.toString()) };
 		} else {
 			machine.chargeCash(requestedSlot.getPrice());
-			machine.refundCash();
+			BigDecimal refund = machine.refundCash();
+			VendingMachineItem item = requestedSlot.removeFirst();
+			
+			return new Message[] {
+				new Message(String.format("Received %s in change", NumberFormat.getCurrencyInstance().format(refund.doubleValue()))),
+				new Message(String.format("Received %s", item.getName()))
+			};
 		}
 	}
 	
 	public void requestChange() { machine.refundCash(); }
 	
 	public void addCash(BigDecimal cash) { machine.addCash(cash); }
+	
+	public BigDecimal getCash() { return machine.getCash(); }
+	
+	public void lookAtAvailableItems() { view.showVendingMachine(); }
 }
