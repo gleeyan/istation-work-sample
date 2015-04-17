78686aa0fc17f7fd93d21aa31d612d70826786f6
lithl <blanketyblankman@gmail.com>
2015-04-16 22:24:41 -0500

    Removed custom machine, streamlined default machine

--- src/bshields/istation/tests/CommandLineTest.java
+++ src/bshields/istation/tests/CommandLineTest.java
@@ -2,10 +2,8 @@ package bshields.istation.tests;
 
 import static java.lang.System.out;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,6 +12,7 @@ import bshields.istation.controllers.VendingMachineController;
 import bshields.istation.interfaces.Shelf;
 import bshields.istation.interfaces.ShelfSlot;
 import bshields.istation.interfaces.VendingMachine;
+import bshields.istation.interfaces.VendingMachineItem;
 import bshields.istation.models.DefaultShelf;
 import bshields.istation.models.DefaultShelfSlot;
 import bshields.istation.models.DefaultVendingMachine;
@@ -30,219 +29,50 @@ public class CommandLineTest {
 	}
 	
 	private static void setupMachine() {
-		char useDefault = (char)0;
-		String line = "";
-		do {
-			out.println();
-			out.print("Do you want to use the default machine [Y], or set up your own [N]? ");
-			line = in.nextLine();
-			if (line.length() > 0) {
-				useDefault = Character.toLowerCase(line.charAt(0));
-			}
-		} while (!(useDefault == 'y' || useDefault == 'n'));
-		
-		if (useDefault == 'y') {
-			List<Shelf> shelves = new ArrayList<Shelf>();
-			List<ShelfSlot> slots = new ArrayList<ShelfSlot>();
-			ShelfSlot slot = new DefaultShelfSlot("C0", new BigDecimal(1.75));
-			slot.addFirst(new DefaultVendingMachineItem("White Cheddar Popcorn"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("C2", new BigDecimal(1.75));
-			slot.addFirst(new DefaultVendingMachineItem("White Cheddar Popcorn"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("C4", new BigDecimal(1.75));
-			slot.addFirst(new DefaultVendingMachineItem("Sea Salt Potato Chips"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("C6", new BigDecimal(1.75));
-			slot.addFirst(new DefaultVendingMachineItem("Sour Cream Potato Chips"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("C8", new BigDecimal(1.75));
-			slot.addFirst(new DefaultVendingMachineItem("Classic Potato Chips"));
-			slots.add(slot);
-			Shelf shelf = new DefaultShelf(slots);
-			shelves.add(shelf);
-			slots = new ArrayList<ShelfSlot>();
-			slot = new DefaultShelfSlot("D0", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("Hard Fruit Candy"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("D1", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("Chocolate & Caramel Sticks"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("D2", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("Rice Chocolate Bar"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("D3", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("W & Ws"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("D4", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("Honey Bar"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("D5", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("White Chocolate Bar"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("D6", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("Chocolate-Covered Coffee Beans"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("D7", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("Gourmet Chocolate Squares"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("D8", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("Soft Fruit Candy"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("D9", new BigDecimal(1.5));
-			slot.addFirst(new DefaultVendingMachineItem("Peanut Butter & Chocolate Bowls"));
-			slots.add(slot);
-			shelf = new DefaultShelf(slots);
-			shelves.add(shelf);
-			slot = new DefaultShelfSlot("E0", new BigDecimal(1.25));
-			slot.addFirst(new DefaultVendingMachineItem("Cinnamon Bun"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("E2", new BigDecimal(1.25));
-			slot.addFirst(new DefaultVendingMachineItem("Corn Chips"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("E4", new BigDecimal(1.25));
-			slot.addFirst(new DefaultVendingMachineItem("Cheesy Chips"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("E6", new BigDecimal(1.25));
-			slot.addFirst(new DefaultVendingMachineItem("Trail Mix"));
-			slots.add(slot);
-			slot = new DefaultShelfSlot("E8", new BigDecimal(1.25));
-			slot.addFirst(new DefaultVendingMachineItem("Potato Strings"));
-			slots.add(slot);
-			shelf = new DefaultShelf(slots);
-			shelves.add(shelf);
-			VendingMachine defaultMachine = new DefaultVendingMachine(new BigDecimal(32.25), shelves);
-			controller = new VendingMachineController(defaultMachine);
-		} else {
-			setupCustomMachine();
+		List<Shelf> shelves = new ArrayList<Shelf>();
+		shelves.add(setupShelf(
+				setupSlot("C0", 1.75, "White Cheddar Popcorn", 3),
+				setupSlot("C2", 1.75, "White Cheddar Popcorn", 5),
+				setupSlot("C4", 1.75, "Sea Salt Potato Chips", 7),
+				setupSlot("C6", 1.75, "Sour Cream Potato Chips", 3),
+				setupSlot("C8", 1.75, "Classic Potato Chips", 1)
+			));
+		shelves.add(setupShelf(
+				setupSlot("D0", 1.5, "Hard Fruit Candy", 9),
+				setupSlot("D1", 1.5, "Chocolate & Caramel Sticks", 5),
+				setupSlot("D2", 1.5, "Rice Chocolate Bar", 13),
+				setupSlot("D3", 1.5, "W & Ws", 4),
+				setupSlot("D4", 1.5, "Honey Bar", 15),
+				setupSlot("D5", 1.5, "White Chocolate Bar", 22),
+				setupSlot("D6", 1.5, "Chocolate-Covered Coffee Beans", 10),
+				setupSlot("D7", 1.5, "Gourmet Chocolate Squares", 1),
+				setupSlot("D8", 1.5, "Soft Fruit Candy", 6),
+				setupSlot("D9", 1.5, "Peanut Butter & Chocolate Bowls", 2)
+			));
+		shelves.add(setupShelf(
+				setupSlot("E0", 1.25, "Cinnamon Bun", 9),
+				setupSlot("E2", 1.25, "Corn Chips", 3),
+				setupSlot("E4", 1.25, "Cheesy Chips", 8),
+				setupSlot("E6", 1.25, "Trail Mix", 11),
+				setupSlot("E8", 1.25, "Potato Strings", 7)
+			));
+		VendingMachine defaultMachine = new DefaultVendingMachine(new BigDecimal(32.25), shelves);
+		controller = new VendingMachineController(defaultMachine);
+	}
+	
+	private static Shelf setupShelf(ShelfSlot...slots) {
+		Shelf shelf = new DefaultShelf();
+		for (ShelfSlot slot : slots) {
+			shelf.addSlot(slot);
 		}
-	}
-	
-	private static void setupCustomMachine() {
-		VendingMachine machine = new DefaultVendingMachine(BigDecimal.ZERO);
-		
-		int selection = 0;
-		do {
-			printShelfMenu();
-			try { selection = Integer.parseInt(in.nextLine()); }
-			catch (NumberFormatException nfe) { continue; }
-			
-			int index = -1;
-			switch (selection) {
-				case 1:
-					machine.addShelf(setupShelf(new DefaultShelf()));
-					break;
-				case 2:
-					List<Shelf> shelves = machine.getShelves();
-					index = getSelectedShelfIndex(shelves) - 1;
-					Shelf editedShelf = setupShelf(shelves.get(index));
-					shelves.set(index, editedShelf);
-					while (machine.getShelves().size() > 0) { machine.removeShelf(0); }
-					
-					Shelf tmp;
-					while ((tmp = shelves.remove(0)) != null) { machine.addShelf(tmp); }
-					break;
-				case 3:
-					index = getSelectedShelfIndex(machine.getShelves()) - 1;
-					machine.removeShelf(index);
-					break;
-			}
-		} while (selection != 4);
-		
-		controller = new VendingMachineController(machine);
-	}
-	
-	private static Shelf setupShelf(Shelf shelf) {
-		int selection = 0;
-		do {
-			printSlotMenu();
-			try { selection = Integer.parseInt(in.nextLine()); }
-			catch (NumberFormatException nfe) { continue; }
-			
-			int index = -1;
-			switch (selection) {
-				case 1:
-					shelf.addSlot(setupSlot(new DefaultShelfSlot("")));
-					break;
-				case 2:
-					List<ShelfSlot> slots = shelf.getSlots();
-					index = getSelectedSlotIndex(shelf.getSlots()) - 1;
-					ShelfSlot editedSlot = setupSlot(slots.get(index));
-					slots.set(index, editedSlot);
-					while (shelf.getSlots().size() > 0) { shelf.removeSlot(0); }
-					
-					ShelfSlot tmp;
-					while ((tmp = slots.remove(0)) != null) { shelf.addSlot(tmp); }
-					break;
-				case 3:
-					index = getSelectedSlotIndex(shelf.getSlots()) - 1;
-					shelf.removeSlot(index);
-					break;
-			}
-		} while (selection != 4);
-		
 		return shelf;
 	}
-	
-	private static ShelfSlot setupSlot(ShelfSlot slot) {
-		
-	}
-	
-	private static int getSelectedSlotIndex(List<ShelfSlot> slots) {
-		
-	}
-	
-	private static int getSelectedShelfIndex(List<Shelf> shelves) {
-		int index = 0;
-		do {
-			printShelfIndices(shelves);
-			try { index = Integer.parseInt(in.nextLine()); }
-			catch (NumberFormatException nfe) { continue; }
-		} while (!(index > 0 && index < shelves.size() + 1));
-		return index;
-	}
-	
-	private static void printSlotIndices(List<ShelfSlot> slots) {
-		out.println();
-		out.println(" ==== Existing Slots");
-		for (int i = 0; i < slots.size(); i++) {
-			out.printf("[%d] %s%s\n",
-					i + 1,
-					slots.get(i).toString().substring(0, 70),
-					slots.get(i).toString().length() > 70 ? "..." : "");
+
+	private static ShelfSlot setupSlot(String keyCode, double price, String name, int count) {
+		ShelfSlot slot = new DefaultShelfSlot(keyCode, new BigDecimal(price));
+		for (int i = 0; i < count; i++) {
+			slot.addLast(new DefaultVendingMachineItem(name));
 		}
-		out.print("> ");
-	}
-	
-	private static void printSlotMenu() {
-		out.println();
-		out.println(" === Slots");
-		out.println("[1] Create new slot");
-		out.println("[2] Edit existing slot");
-		out.println("[3] Remove slot");
-		out.println("[4] Finished");
-		out.print("> ");
-	}
-	
-	private static void printShelfIndices(List<Shelf> shelves) {
-		out.println();
-		out.println(" === Existing Shelves");
-		for (int i = 0; i < shelves.size(); i++) {
-			out.printf("[%d] %s%s\n",
-					i + 1,
-					shelves.get(i).toString().substring(0, 70),
-					shelves.get(i).toString().length() > 70 ? "..." : "");
-		}
-		out.print("> ");
-	}
-	
-	private static void printShelfMenu() {
-		out.println();
-		out.println(" == Shelves");
-		out.println("[1] Create new shelf");
-		out.println("[2] Edit existing shelf");
-		out.println("[3] Remove shelf");
-		out.println("[4] Finished");
-		out.print("> ");
+		return slot;
 	}
 }
